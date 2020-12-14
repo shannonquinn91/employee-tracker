cd190408da28bc31d7021d11df8467e77a93db56
Shannon Quinn <shannon.quinn91@gmail.com>
2020-12-14 14:50:18 -0500

    Update to schemas with foreign keys

--- schemas.sql
+++ schemas.sql
@@ -4,27 +4,28 @@ create database employee_DB;
 
 use employee_DB;
 
+
 create table department(
-id integer auto_increment primary key,
-name varchar(30)
+department_id integer auto_increment,
+name varchar(30),
+primary key (department_id)
 );
 
 create table role(
-id integer auto_increment primary key, 
+role_id integer auto_increment, 
 title varchar(30),
-salary decimal(10,2),
-department_id integer, 
-foreign key (department_id) references department (id)
+salary decimal(10,2), 
+department_id int,
+foreign key (department_id) references department(department_id),
+primary key (role_id)
 );
 
 create table employee(
-id integer auto_increment primary key,
+id integer auto_increment,
 first_name varchar(30),
 last_name varchar(30),
-role_id integer,
-manager_id integer,
-foreign key (role_id) references role (id),
-foreign key (manager_id) references role (id)
+role_id int,
+foreign key (role_id) references role(role_id),
+primary key (id)
 );
-
 
